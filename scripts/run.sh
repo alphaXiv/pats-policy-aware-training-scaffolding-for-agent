6a26af4449075e19511ed540a3d2026922d45208
Rehaan Ahmad <rehaanahmad2013@gmail.com>
2026-07-26 07:21:03 -0700

    Install ALFWorld native build prerequisites

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,6 +6,8 @@ export HF_HUB_ENABLE_HF_TRANSFER=1
 export TOKENIZERS_PARALLELISM=false
 export PYTHONUNBUFFERED=1
 
+apt-get update -qq
+DEBIAN_FRONTEND=noninteractive apt-get install -y -qq build-essential
 python -m pip install --disable-pip-version-check -r requirements.txt
 if [ ! -f "${ALFWORLD_DATA}/logic/alfred.pddl" ]; then
   alfworld-download
